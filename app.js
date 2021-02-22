5eb5e0bec4766a8d147e5a9f6097e4c6ecd1b37f
tanjilk <tanjilkh@gmail.com>
2021-02-22 10:05:35 +0000

    changed to eng language

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const argum = process.argv[2];
 // If the argument doesnt pass
 
 if(!argum){
-	console.log('Necessita de passar o username como um argumento.');
+	console.log('You need to pass the username as argument!');
 }
 
 else {
@@ -16,11 +16,10 @@ else {
 	const SteamUser = require("steam-user");
 	const readlineSync = require("readline-sync");
 
-	
 
 	const client = new SteamUser();
 
-	console.log("\x1b[33m", "Por favor, faça login ao digitar os seus credenciais: ");
+	console.log("\x1b[33m", "Please login with the credentials: ");
 	console.log("\x1b[37m", " ");
 
 	var password = readlineSync.question("Password: ", {
@@ -35,7 +34,7 @@ else {
 
 		client.logOn(logOnOptions);
 		client.on('error', function(e){
-			console.log('Um erro aconteceu, por favor tente de novo.');
+			console.log('Wrong password, please try again.');
 		});
 		
 	}
@@ -43,36 +42,36 @@ else {
 	login(argum, password);
 
 	client.on('loggedOn', () => {
-		console.log("\x1b[32m","Login feito com sucesso. ✔️");
-		console.log("\x1b[37m", "Define o status");
+		console.log("\x1b[32m","Login successful. ✔️");
+		console.log("\x1b[37m", "Define your status: ");
 		console.log('\x1b[36m%s\x1b[0m', "1. Online\n2. Offline");
 		
-		var status = readlineSync.question("Escolha o status: ");
+		var status = readlineSync.question("Choose the status: ");
 		if(status == "1"){
 			client.setPersona(SteamUser.EPersonaState.Online);
 			function wait(){
-				console.log("O seu perfil foi definido como ONLINE")	
+				console.log("Your profile was defined as: ONLINE")	
 			}
 			setTimeout(wait, 3000);
 			
 		} else if(status == "2"){
 			client.setPersona(SteamUser.EPersonaState.Offline);
 			function wait1(){
-				console.log("O seu perfil foi definido como OFFLINE")
+				console.log("Your profile was defined as: OFFLINE")
 			}
 			setTimeout(wait1, 3000);
 		} else {
 			function waiterr(){
-				console.log("\x1b[31m", "Um erro inesperado aconteceu. Tente outra vez. ❗")	
+				console.log("\x1b[31m", "An error happened. Please try again ❗")	
 			}
 			setTimeout(waiterr, 2000)
 			
 		}
 		
-		console.log("\x1b[37m", "Introduza os App ID dos jogos que gostaria de farmar");
-		console.log("\x1b[37m", "Exemplo: 730, 440, 240")
-		console.log("\x1b[33m", "Máximo: 25 jogos ⚠️")
-		var games = readlineSync.question("Digite os IDs: ");
+		console.log("\x1b[37m", "Introduce the steam app ids to farm: ");
+		console.log("\x1b[37m", "Example: 730, 440, 240, 218620")
+		console.log("\x1b[33m", "Max: 25 jogos ⚠️")
+		var games = readlineSync.question("Type the steam IDS: ");
 		var array = games.split(',');
 		games = [];
 		for(i = 0; i < array.length; i++){
@@ -81,37 +80,37 @@ else {
 		}
 		client.gamesPlayed(games);
 		if(!games){
-			console.log('Não foi introduzido nenhum jogo')
+			console.log('No games was introduced... Skipping!')
 		}
-		console.log("Jogos que foram adicionados: " + games);
-		console.log("\x1b[32m", "Jogos recebidos com sucesso. ✔️")
-		console.log("\x1b[32m", "Configurações básicas efetuada com sucesso. ✔️")
+		console.log("Added games to farm: " + games);
+		console.log("\x1b[32m", "Games added successful. ✔️")
+		console.log("\x1b[32m", "Configurations defined with success. ✔️")
 		
-		console.log("\x1b[37m", "Gostaria de querer uma resposta automática? [y/n]")
+		console.log("\x1b[37m", "Would you like to autommatic response bot? [y/n]")
 		var auto = readlineSync.question(": ");
 		if(auto == "y"){
-			console.log("Exemplo: Olá. sou um bot do steam noodles! :D")
-			console.log("Exemplo: Olá. estou ocupado! Não fale comigo agora :D")
-			var choice = readlineSync.question("Escreva uma frase que responde automáticamente se alguém mandar mensagem para ti: ")
-			console.log("O bot irá responder " + choice)
+			console.log("Exemple: Hi, i am a bot and i do bot stuff :D")
+			console.log("Exemplo: Im sleeping! Dont send messages to me :P")
+			var choice = readlineSync.question("Write an autommatic response:  ");
+			console.log("The bot will reply " + choice)
 			function replySuc(){
-				console.log("\x1b[32m", "Configurado com sucesso ✔️")
+				console.log("\x1b[32m", "Configurations defined ✔️")
 				}
 				client.on("friendMessage", function(steamID, message){
-					console.log("\x1b[35m", "Mensagem recebida pelo " + steamID + ": " + message);
+					console.log("\x1b[35m", "Message received from " + steamID + ": " + message);
 					if(message){
 						client.chatMessage(steamID, choice);
 					}
 				})
 			setTimeout(replySuc, 3000);
 		} else if(auto == "n"){
-			console.log("O bot vai ficar em silencio.")
+			console.log("Bot will be quiet.")
 		} else{
-			console.log("\x1b[31m", "Um erro inesperado aconteceu. Tente outra vez. ❗");
+			console.log("\x1b[31m", "An error happened❗");
 		}
 
 		client.on("friendMessage", function(steamID, message){
-			console.log("\x1b[35m", "Mensagem recebida pelo " + steamID + ": " + message);
+			console.log("\x1b[35m", "Message received from " + steamID + ": " + message);
 		})
 			
 	})
